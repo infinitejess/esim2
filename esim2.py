5a6b77f731e40f5aeef053211c91783859819b14
Jessica Long <jess@jessicalong.dev>
2023-12-21 21:38:29 -0800

    Simplify option parsing, reflow comment

--- esim2.py
+++ esim2.py
@@ -10,31 +10,36 @@ ester options are EV (valerate), EEn (enanthate), EB (benzoate), EC (cypionate),
 userinput = '0/4/EV, 7/4/een, 14/2.5/een, 21/1.5/een, 28/4/ev, 35/4/een, 42/2.5/een, 49/1.5/een'
 
 #ester a, b, c, d values (used in calculations)
-een = [0.42412968, 0.43452980, 0.15291485, 333.874181]
-ev = [2.38229125, 0.23345814, 1.37642769, 2596.05956]
-eun = [0.29634323, 4799337.57, 0.03141554, 65.9493374]
-eb = [3.22397192, 0.58870148, 70721.4018, 170500000]
-ec = [0.29634323, 4799337.57, 0.03141554, 65.9493374]
+esters = {
+    'een': [0.42412968, 0.43452980, 0.15291485, 333.874181],
+    'ev': [2.38229125, 0.23345814, 1.37642769, 2596.05956],
+    'eun': [0.29634323, 4799337.57, 0.03141554, 65.9493374],
+    'eb': [3.22397192, 0.58870148, 70721.4018, 170500000],
+    'ec': [0.29634323, 4799337.57, 0.03141554, 65.9493374],
+}
 #apparently ur supposed to use EEn instead of EC when ur graphing bause EC data is flawed, idk just doing what im told
 
-'''
-turns string into a properly formatted list of the following form (its like this so i can use it elsewhere easily)
-[
-[day of injection (counting up from start date, starting at 0), dose taken on that date, ester injected (een/ev/eu/ec)]
-]
-'''
-injections = []
-for term in [term.strip(" ()[]").lower() for term in userinput.split(',')]:
-    a = term.split('/')
-    a[0] = float(a[0])
-    a[1] = float(a[1])
-    match a[2]:
-        case 'een': a[2] = een
-        case 'ev': a[2] = ev
-        case 'ec': a[2] = ec
-        case 'eun': a[2] = eu
-        case _: a[2] = een #defaults to EEn
-    injections.append(a)
+def parse_injections(userinput):
+    '''
+        turns string into a properly formatted list of the following form (its
+        like this so i can use it elsewhere easily)
+        [
+        [
+            day of injection (counting up from start date, starting at 0),
+            dose taken on that date,
+            ester injected (een/ev/eu/ec)]
+        ]
+    '''
+    result = []
+    for term in [term.strip(" ()[]").lower() for term in userinput.split(',')]:
+        a = term.split('/')
+        a[0] = float(a[0])
+        a[1] = float(a[1])
+        a[2] = esters[a[2]]
+        result.append(a)
+    return result
+
+injections = parse_injections(userinput)
 
 '''
 basically the desmos calc but in python
